06c3d9daaba95ff76665a27c46f6e860db9d9745
Hyunstone <hyun104467@naver.com>
2022-12-25 15:24:33 +0900

    챕터4

--- src/main/java/Hyunstone/springboot/service/posts/PostsService.java
+++ src/main/java/Hyunstone/springboot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package Hyunstone.springboot.service.posts;
 
 import Hyunstone.springboot.domain.posts.Posts;
 import Hyunstone.springboot.domain.posts.PostsRepository;
+import Hyunstone.springboot.web.dto.PostsListResponseDto;
 import Hyunstone.springboot.web.dto.PostsResponseDto;
 import Hyunstone.springboot.web.dto.PostsSaveRequestDto;
 import Hyunstone.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,5 +37,19 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id =" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+        postsRepository.delete(posts);
     }
 }
