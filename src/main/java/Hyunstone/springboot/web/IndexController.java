84b1572fcd59893b745d42a8afbc63267105b297
Hyunstone <hyun104467@naver.com>
2022-12-26 01:15:04 +0900

    챕터5 완료

--- src/main/java/Hyunstone/springboot/web/IndexController.java
+++ src/main/java/Hyunstone/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package Hyunstone.springboot.web;
 
+import Hyunstone.springboot.config.auth.LoginUser;
+import Hyunstone.springboot.config.auth.dto.SessionUser;
 import Hyunstone.springboot.service.posts.PostsService;
 import Hyunstone.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -13,9 +15,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class IndexController {
 
     private final PostsService postsService;
+
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
